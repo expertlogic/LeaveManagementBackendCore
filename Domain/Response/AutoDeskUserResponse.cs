6a7a988510601ac8406460121990657e52d81ecd
expertlogic <se.expertlogic@gmail.com>
2023-10-15 02:11:55 +0400

    changes

---
+++ Domain/Response/AutoDeskUserResponse.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Response
+{
+    
+    public class AutoDeskUserResponse
+    {
+        public string sub { get; set; }
+        public string name { get; set; }
+        public string given_name { get; set; }
+        public string family_name { get; set; }
+        public string preferred_username { get; set; }
+        public string email { get; set; }
+        public bool email_verified { get; set; }
+        public string profile { get; set; }
+        public string picture { get; set; }
+        public string locale { get; set; }
+        public int updated_at { get; set; }
+    }
+
+    public class AutoDeskAccessTokenResponse
+    {
+        public string access_token { get; set; }
+        public string token_type { get; set; }
+        public int expires_in { get; set; }
+        public string refresh_token { get; set; }
+    }
+}
